d81efa7d564639b3a816b73157fd231c4d82585b
FirstSymbol <firstsymbol.0@gmail.com>
2025-05-29 12:54:01 +0300

    Добавил инициализатор камеры для фолоу плеер.

---
+++ Assets/ProjectContent/Code/PrototypingFolder/CameraInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Unity.Cinemachine;
+using Unity.VisualScripting;
+using UnityEngine;
+using Zenject;
+
+namespace ProjectContent.Code.PrototypingFolder
+{
+  public class CameraInitializer : MonoBehaviour
+  {
+    public CinemachineCamera CinemachineCamera;
+    private CreatureFabric _creatureFabric;
+
+    [Inject]
+    private void Inject(CreatureFabric creatureFabric)
+    {
+      _creatureFabric = creatureFabric;
+    }
+
+    private void Start()
+    {
+      _creatureFabric.OnPlayerCreated = OnPlayerCreated;
+    }
+
+    private void OnPlayerCreated()
+    {
+      if (CinemachineCamera != null)
+      {
+        CinemachineCamera.Follow = _creatureFabric.Player.transform;
+      }
+    }
+  }
+}
